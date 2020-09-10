60633d4926fac8e437c0e6e3edcbbe223173b285
Ryan McGovern <tekgadgt@devportfol.io>
2020-09-10 14:48:39 -0400

    added accent-green color to tailwind, applied it to all buttons, fixed postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-const tailwindcss = require('tailwindcss');
+// const tailwindcss = require('tailwindcss');
 module.exports = {
-    plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+    plugins: [require('tailwindcss'), require('autoprefixer')],
 };
